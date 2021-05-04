5ae2ea4a70c0723e31dbfafd692860c602a64511
Darshan99599 <83588242+Darshan99599@users.noreply.github.com>
2021-05-04 16:33:07 +0530

    Add files via upload

---
+++ hospital management system/login.js
@@ -0,0 +1,18 @@
+function login()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+if (username=="190031815@kluniversity.in" && password=="31815")
+{
+    alert("login successful");
+}
+else if (username=="190031686@kluniversity.in" && password=="31686"){
+    alert("login successful");
+}
+else if (username=="190031651@kluniversity.in" && password=="31651"){
+    alert("login successful");
+}
+else{
+    alert("login failed");
+}
+}
